ac5e6651f1834d623527999b280aab2df9ce96d8
sajid <s.rahman@einhundert-energie.de>
2023-10-29 14:52:42 +0100

    tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ProductTests(TestCase):
+
+    def test1(self):
+        from main.models import Product
+
+        assert Product.objects.count() == 0
+        Product.objects.create(
+            title="title 1"
+        )
+        assert Product.objects.count() == 1
